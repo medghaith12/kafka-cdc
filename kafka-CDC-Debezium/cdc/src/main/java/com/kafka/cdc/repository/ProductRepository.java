9714e8f92eb752f5dd88a2025ec2029517a8390a
medghaith12 <157299425+medghaith12@users.noreply.github.com>
2024-02-16 11:14:44 +0100

    Delete kafka-CDC-Debezium directory

--- kafka-CDC-Debezium/cdc/src/main/java/com/kafka/cdc/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kafka.cdc.repository;
-
-import com.kafka.cdc.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
